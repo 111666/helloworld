f557797553973dacf3c9dc91d7f7beb38c6d0924
Better <309650860@qq.com>
2018-04-14 13:30:12 +0800

    see

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static void hello() {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+    public static void main(String[] args) {
+        Main.hello();
     }
 }
